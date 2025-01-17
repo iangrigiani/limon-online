ad0939a9e9a74afa696225c1ea8a997849c2aecf
Ignacio Angrigiani <ignacio.angrigiani@gmail.com>
2025-01-16 23:14:53 -0300

    Delete app.listen

--- server/src/app.js
+++ server/src/app.js
@@ -18,7 +18,8 @@ app.use(express.json());
 app.use('/api/game', gameRoutes);
 app.use('/api/highscores', highscoresRoutes);
 
+/*
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
-}); 
+}); */
